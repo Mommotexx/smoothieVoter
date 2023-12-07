950ab46fa245661168e98901aa0b046d5df4a754
Marcus Vinje Johansen <tex@duck.com>
2023-12-07 22:24:41 +0100

    Dealing with eslint

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <section className="mt-16 flex items-center flex-col gap-3">
+    <section className="mt-16 flex flex-col items-center gap-3">
       <h1 className="text-4xl">Welcome to the smoothie voting app</h1>
       <p>Please login to check out and vote for the smoothies</p>
     </section>
