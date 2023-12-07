60e1e501c318abb8daaaefcde0cb51e30e8caec9
Marcus Vinje Johansen <tex@duck.com>
2023-12-07 22:50:35 +0100

    'Done' with the home page. Don't want to style it as of now

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <section className="mt-16 flex flex-col items-center gap-3">
       <h1 className="text-4xl">Welcome to the smoothie voting app</h1>
-      <p>Please login to check out and vote for the smoothies</p>
+      <p>
+        Please login to check out and vote for the smoothies you love the most
+      </p>
+      <Button>
+        <Link href={"/sign-in"}>Log In</Link>
+      </Button>
     </section>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,29 @@
+import { SignedIn, SignedOut, UserProfile } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center px-6 py-3"></nav>
+    <nav className="flex flex-row items-center justify-between py-3 md:px-6">
+      <Link href="/">
+        <Image
+          src={"/assets/images/smoothie.svg"}
+          alt="smoothie logo"
+          width={50}
+          height={50}
+        />
+      </Link>
+      <SignedOut>
+        <Button>
+          <Link href={"/sign-in"}>Log In</Link>
+        </Button>
+      </SignedOut>
+      <SignedIn>
+        <UserProfile />
+      </SignedIn>
+    </nav>
   );
 };
 
